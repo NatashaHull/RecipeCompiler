f748e7b7f8e68160503aa58bec906d27325859cf
Natasha A. Hull <musicalnat@gmail.com>
2013-03-08 21:28:28 -0800

    Started new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecipeCompiler::Application.config.secret_token = '810c9e4d16f9f739e502ce605072fe487442d9633f27ea42b3378c62aadc6f50d25319ace30c2de154cd63e1b8ebbd862dab149b13256375cf1c0871e496a836'
